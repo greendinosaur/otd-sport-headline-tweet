1b5cd460b4b715923ece2b655cd728d55d45e46f
greendinosaur <greendinosaur@users.noreply.github.com>
2020-05-26 20:56:34 -0700

    updated to use AWS KMS

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,19 @@
+import boto3
+import os
+
+from base64 import b64decode
+
 from otd import onthisday, tweet
 
 print('Loading function')
+
+
+
+CONSUMER_KEY = boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['TWITTER_CONSUMER_KEY']))['Plaintext'].decode('utf-8')
+CONSUMER_SECRET = boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['TWITTER_CONSUMER_SECRET']))['Plaintext'].decode('utf-8')
+ACCESS_TOKEN = boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['TWITTER_ACCESS_TOKEN']))['Plaintext'].decode('utf-8')
+ACCESS_TOKEN_SECRET = boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['TWITTER_ACCESS_TOKEN_SECRET']))['Plaintext'].decode('utf-8')
+
 
 
 def lambda_handler(event, context):
@@ -10,7 +23,11 @@ def lambda_handler(event, context):
     # if you don't want it to tweet then setup a key called test inside the event
     if "test" not in event and headline != onthisday.NO_MATCH_HEADLINE:
         print("tweeting the headline")
-        tweet.tweet_headline(headline)
+        tweet.tweet_headline(headline, 
+                            consumer_key=CONSUMER_KEY,
+                            consumer_secret=CONSUMER_SECRET,
+                            access_token=ACCESS_TOKEN,
+                            access_token_secret=ACCESS_TOKEN_SECRET)
     else:
         print("not tweeting")
     return headline
--- otd/tweet.py
+++ otd/tweet.py
@@ -2,15 +2,21 @@ import tweepy
 import config
 
 
-def tweet_headline(message):
+def tweet_headline(message,
+                   consumer_key = config.CONSUMER_KEY,
+                   consumer_secret=config.CONSUMER_SECRET,
+                   access_token=config.ACCESS_TOKEN,
+                   access_token_secret=config.ACCESS_TOKEN_SECRET):
     """ tweets the provided message to twitter
         message is the status message to tweet
+        can call with the twitter keys directly, otherwise, defaults
+        to the values stored in the config
     """
     # Authenticate to Twitter
-    auth = tweepy.OAuthHandler(config.CONSUMER_KEY,
-                               config.CONSUMER_SECRET)
-    auth.set_access_token(config.ACCESS_TOKEN,
-                          config.ACCESS_TOKEN_SECRET)
+    auth = tweepy.OAuthHandler(consumer_key,
+                               consumer_secret)
+    auth.set_access_token(access_token,
+                          access_token_secret)
 
     # Create API object
     api = tweepy.API(auth)
